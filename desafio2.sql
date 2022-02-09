018cc4537b5e6f3ec207335c585ff69e04c009a6
Gabriel Takaki <gabriel.f.takaki@gmail.com>
2022-02-09 18:56:17 -0300

    requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 SELECT 
-COUNT(DISTINCT song.song_id) AS 'cancoes', 
-COUNT(DISTINCT artist.artist_id) AS 'artistas',
+COUNT(DISTINCT song.cancao_id) AS 'cancoes', 
+COUNT(DISTINCT artist.artista_id) AS 'artistas',
 COUNT(DISTINCT album.album_id) AS 'albuns'
 FROM 
-	SpotifyClone.songs as song
-    INNER JOIN SpotifyClone.artists AS artist
+	SpotifyClone.cancoes as song
+    INNER JOIN SpotifyClone.artistas AS artist
     INNER JOIN SpotifyClone.albums AS album;
