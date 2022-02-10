f79ad85bd6476711caf458947c7175bf2e8f6cec
Gabriel Takaki <gabriel.f.takaki@gmail.com>
2022-02-09 23:57:27 -0300

    req 8

--- desafio8.sql
+++ desafio8.sql
@@ -5,5 +5,4 @@ AL.album AS 'album'
 
 FROM SpotifyClone.artistas AS A
 JOIN SpotifyClone.albums AS AL
-ON A.artista_id = AL.artista_id WHERE A.artista = "Walter Phoenix"
-ORDER BY artista ASC;
+ON A.artista_id = AL.artista_id WHERE A.artista = "Walter Phoenix";
